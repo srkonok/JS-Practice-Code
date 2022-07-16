ccf0094ab22f37cb0d6ed6c6b2e951d5aa3054fb
konok2014 <64254730+konok2014@users.noreply.github.com>
2022-07-16 16:08:15 +0600

    Functional_practice16072022

---
+++ 02_Function/script.js
@@ -0,0 +1,31 @@
+'use strict';
+//default parameter 
+const bookings=[]
+const createBooking =function (flightNum,numPassengers=1,price=199*numPassengers){
+const booking={
+    flightNum,
+    numPassengers,
+    price,
+}
+console.log(booking);
+bookings.push(booking);
+}
+
+createBooking('DHAKA450',2)
+createBooking('DHAKA450',5,1300)
+createBooking('DHAKA450',undefined,100)
+
+//How Passing argument Works value vs Reference
+
+const flight ='DHAKA24'
+const Konok={
+    name: "Shahariar Rahman",
+    passport: 2547845232,
+}
+
+const addMd=function(flight,passenger){
+    flight='KUSHTIA54';
+    passenger.name='Md.'+passenger.name;
+}
+console.log(flight);//will not change bcz it primitive value
+console.log(Konok);// change because it is reference
